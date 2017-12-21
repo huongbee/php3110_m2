f05de959edf9d8225b67c9c00d62203e9f05d7fc
Huong <you@example.com>
2017-12-21 21:23:30 +0700

    end

--- uploadfile/upload-multiple.php
+++ uploadfile/upload-multiple.php
@@ -13,9 +13,9 @@ date_default_timezone_set("Asia/Ho_Chi_Minh");
 
 if(isset($_POST['btnUpload'])){
     $file = $_FILES['image'];  
-    echo "<pre>";  
-    print_r($file);
-    echo "</pre>";
+    // echo "<pre>";  
+    // print_r($file);
+    // echo "</pre>";
     
     //kiem tra filezise <=1mb
     //chi cho phep upload hinh png, jpg, gif, jpeg
@@ -24,6 +24,11 @@ if(isset($_POST['btnUpload'])){
 
 
     //tmpname, nameFile
+    $tmpName = $file['tmp_name'];
+    foreach($tmpName as $key=>$tmp){
+        $name = $file['name'];
+        move_uploaded_file($tmp,"images/".$name[$key]);
+    }
 }
 
 ?>
