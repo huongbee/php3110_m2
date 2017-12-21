8d724cdc0318913e2dcccdd24b52cef85c4e4a90
Huong <you@example.com>
2017-12-21 20:28:59 +0700

    uploadfile

--- uploadfile/index.php
+++ uploadfile/index.php
@@ -7,9 +7,46 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+if(isset($_POST['btnUpload'])){
+
+    if($_FILES['image']['name']==''){
+        echo "dont choose file";
+        return;
+    }
+    $file = $_FILES['image'];  
+    // echo "<pre>";  
+    // print_r($file);
+    // echo "</pre>";
+
+    $tmpName = $file['tmp_name'];
+    $name = $file['name'];
+
+    //kiem tra filezise <=1mb
+    //chi cho phep upload hinh png, jpg, gif, jpeg
+    //rename file
+    
+    $size = $file['size'];
+    if($size>1024*1024){
+        echo "File too large! Choose another";
+        die;
+    }
+    //a.png
+    //a.jpeg
+    
+    echo pathinfo($name,PATHINFO_EXTENSION);
 
 
-    <form method="POST" encrypt="multiple/form-data">
+
+    //move_uploaded_file($tmpName,"images/$name");
+    //echo "success";
+
+}
+
+
+?>
+
+    <form method="post" enctype="multipart/form-data">
         <input type="file" name="image">
         <button type="submit" name="btnUpload">Upload</button>
     </form>
