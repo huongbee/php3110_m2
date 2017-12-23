8e3fde3752a0f7a7a974105c18f63ba47f7c8764
Huong <you@example.com>
2017-12-23 20:01:45 +0700

    rand

---
+++ buoi4/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//echo rand(100000,999999);
+$arr = ['PHP','iOS','Android','NodeJS','Angular','React','Front-end'];
+//echo $arr[1]; //7
+//echo $arr[rand(0,count($arr)-1)];
+
+
+$str = "ZXCVBNMASDFGHJKLQWERTYUIOPqwertyuiopasdfghjklzxcvbnm1234567890";
+
+echo $str[61];
+
+
+?>
--- uploadfile/upload-multiple.php
+++ uploadfile/upload-multiple.php
@@ -10,6 +10,7 @@
 <?php
 
 date_default_timezone_set("Asia/Ho_Chi_Minh");
+//unlink('images/download.png');
 
 if(isset($_POST['btnUpload'])){
     $file = $_FILES['image'];  
@@ -21,15 +22,40 @@ if(isset($_POST['btnUpload'])){
     //chi cho phep upload hinh png, jpg, gif, jpeg
     //rename file
     //neu ma co 1 trong cac file upload ko thanh cong, rollback 
+    $size = $file['size'];
+    foreach($size as $s){
+        if($s>1024*1024){
+            die('File too large!');
+        }
+    }
+
+    $arrExt = ['png', 'jpg', 'gif', 'jpeg'];
+    $name = $file['name'];
+    foreach($name as $ten){
+        $ext = strtolower(pathinfo($ten,PATHINFO_EXTENSION));
+        if(!in_array($ext,$arrExt)){
+            echo "File khong duoc phep chon";
+            die;
+        }
+    }
+    
+    //uploadfile
+    
+
 
 
     //tmpname, nameFile
     $tmpName = $file['tmp_name'];
     foreach($tmpName as $key=>$tmp){
         $name = $file['name'];
-        move_uploaded_file($tmp,"images/".$name[$key]);
+        move_uploaded_file($tmp,"images/".time().'-'.$name[$key]);
     }
+
+
 }
+
+
+
 
 ?>
 
