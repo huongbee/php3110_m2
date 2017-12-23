f47908409074c2e876e42befc8cd46c5f090338f
Huong <you@example.com>
2017-12-23 20:10:43 +0700

    createToken

--- buoi4/index.php
+++ buoi4/index.php
@@ -6,9 +6,21 @@ $arr = ['PHP','iOS','Android','NodeJS','Angular','React','Front-end'];
 //echo $arr[rand(0,count($arr)-1)];
 
 
-$str = "ZXCVBNMASDFGHJKLQWERTYUIOPqwertyuiopasdfghjklzxcvbnm1234567890";
+//echo $str[61];
 
-echo $str[61];
+function createToken($length){
+    $str = "ZXCVBNMASDFGHJKLQWERTYUIOPqwertyuiopasdfghjklzxcvbnm1234567890";
+    $token = '';
+    for($i=1;$i<=$length;$i++){
+        $pos = rand(0,strlen($str)-1);
+        $token.=$str[$pos];
+    }
+    return $token;
+}
 
+echo createToken(35);
+
+//CE3lx7hQtsytFXAhlLl2Twk2wEpfd7
+//p8qHGkMZAg3ce4kuXICmRbKsoF8yibQn4Ln
 
 ?>
