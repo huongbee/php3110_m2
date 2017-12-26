19f3bdf4f71772413189e501220cf8559d184e2c
Huong <you@example.com>
2017-12-26 20:18:01 +0700

    oop1

--- oop/2.php
+++ oop/2.php
@@ -1,10 +1,14 @@
 <?php
 class SinhVien{
     public $name = "Khoa pham training";
-    public $id = "234567";
+    private $id = "234567";
 
     public function setName($ten){
         $this->name = $ten;
+    }
+
+    function getId(){
+        return $this->id;
     }
 
 }
@@ -12,15 +16,22 @@ class HocSinh extends SinhVien{
 
     public $name;
     public $age;
+    
     public function setAge($tuoi){
         $this->age = $tuoi;
     }
+
+    public function getId(){
+        return $this->id;
+    }
     
 }
-$svB = new HocSinh;
-echo  $svB->name;
-$svb->setName('Nguyen Van A');
-echo $svB->name;
+// $svB = new HocSinh;
+// echo  $svB->name;
+// echo "<br>";
+// echo $svB->getId();
+// $svB->setName('Nguyen Van A');
+// echo $svB->name;
 
 
 
@@ -29,9 +40,46 @@ echo $svB->name;
 // $svA->setName('Nguyen Van A');
 // echo "<br>";
 // echo  $svA->name;
+// //echo $svA->id;
+// echo $svA->getId();
+
+/**
+ * create class Cat have propertis:name,color
+ *                  have action: go, eat, catchMouse
+ * create class son's Cat except catchMouse
+ * class Chihuahua 
+ */
+class Cat{
+    protected $name;
+    protected $color = "black";
+    
+    protected function go(){
+        return $this->getName()." going to school.";
+    }
+    protected function setName($name){
+        return $this->name = $name;
+    }
+    protected function getName(){
+        return $this->name;
+    }
+    private function catchMouse(){
+        return "con mèo mẹ đang bắt con chuột";
+    }
+
+}
+class subCat extends Cat{
+    protected function play(){
+        return $this->name. " playing soccer";
+    }
+   // color, name, play, go, setName, getName
+
+}
+class Chihuahua extends subCat{
+    //color, name, play, go, setName, getName
+}
 
 
-
-
-
+$chi1 = new Chihuahua;
+$chi1->setName('HuaHua');
+echo $chi1->getName();
 ?>
